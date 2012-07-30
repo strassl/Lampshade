55d4e24c30164c4a7a482d63511096b16c7bec66
Simon Strassl <simon.strassl@gmail.com>
2012-07-30 12:40:52 +0200

    Remove old update checker

--- Lampshade/src/eu/prismsw/lampshade/MainActivity.java
+++ Lampshade/src/eu/prismsw/lampshade/MainActivity.java
@@ -1,10 +1,7 @@
 package eu.prismsw.lampshade;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
@@ -16,14 +13,11 @@ import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 
-import eu.prismsw.lampshade.fragments.AlertDialogFragment;
 import eu.prismsw.lampshade.fragments.SearchFragment;
 import eu.prismsw.tropeswrapper.TropesHelper;
 
 /** All other activities are accessed from this one */
 public class MainActivity extends BaseActivity {
-	private static String KEY_UPDATE = "lastUpdate";
-	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -91,60 +85,5 @@ public class MainActivity extends BaseActivity {
 				searchIntent.putExtra(SearchFragment.QUERY_KEY, query);
 				searchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 				startActivity(searchIntent);
-    }
-    
-    private void tryUpdateCheck() {
-    	SharedPreferences prefs = getPreferences(0);
-        Long lastUpdate =  prefs.getLong(KEY_UPDATE, MODE_PRIVATE);
-        
-        Long nextUpdateTime = lastUpdate + (24 * 60 * 60 * 1000);
-        Long currentTime = System.currentTimeMillis();
-        if (nextUpdateTime < currentTime) {
-            SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
-            editor.putLong(KEY_UPDATE, currentTime);
-            editor.commit();        
-
-	        new CheckUpdateTask(this).execute(Uri.parse(TropesApplication.versionUrl));
-        }
-
-    }
-    
-    private class CheckUpdateTask extends AsyncTask<Uri, Integer, VersionInformation> {
-    	public BaseActivity activity;
-    	
-    	public CheckUpdateTask(BaseActivity activity) {
-    		this.activity = activity;
-    	}
-
-		@Override
-		protected VersionInformation doInBackground(Uri... params) {
-			VersionInformation newestVersion = getNewestVersion(params[0]);
-			return newestVersion;
-		}
-		
-		@Override
-		protected void onPostExecute(VersionInformation newestVersion) {
-	    	Integer currentVersion = getApplicationVersion();
-	    	
-	    	if(currentVersion > -1 && newestVersion.versionNumber > -1 && newestVersion.versionNumber > currentVersion) {
-				AlertDialogFragment f = AlertDialogFragment.newInstance(newestVersion.versionString + " available", newestVersion.information + "<br /><br /><strong>Changelog</strong><br />" + newestVersion.changelog);
-				activity.showDialogFragment(f);
-			}
-		}
-    	
-	    private Integer getApplicationVersion() {
-	    	Integer versionNumber = -1;
-			try {
-				PackageInfo pInfo = activity.getPackageManager().getPackageInfo(getPackageName(), MODE_PRIVATE);
-		    	versionNumber = pInfo.versionCode;
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		    return versionNumber;
-	    }
-	    
-	    private VersionInformation getNewestVersion(Uri url) {
-	    	return new VersionInformation(url);
-	    }
     }
 }
