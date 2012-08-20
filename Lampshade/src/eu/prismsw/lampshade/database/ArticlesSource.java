dace72e191dd5866891af831fc29dc5251124ef9
Simon Strassl <simon.strassl@gmail.com>
2012-08-20 18:12:01 +0200

    Clean up ArticlesSource and add new helper functions

--- Lampshade/src/eu/prismsw/lampshade/database/ArticlesSource.java
+++ Lampshade/src/eu/prismsw/lampshade/database/ArticlesSource.java
@@ -9,17 +9,16 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
 
 /** Wrapper for common database functions */
 public class ArticlesSource {
 	
 	protected SQLiteDatabase database;
-	protected SQLiteOpenHelper helper;
-	protected String[] allColumns = {SavedArticlesHelper.COLUMN_ID, SavedArticlesHelper.COLUMN_TITLE, SavedArticlesHelper.COLUMN_URL};
+	protected SavedArticlesHelper helper;
+	protected String[] infoColumns = {SavedArticlesHelper.ARTICLES_COLUMN_ID, SavedArticlesHelper.ARTICLES_COLUMN_TITLE, SavedArticlesHelper.ARTICLES_COLUMN_URL};
 	
-	public ArticlesSource(SQLiteOpenHelper helper) {
+	public ArticlesSource(SavedArticlesHelper helper) {
 		this.helper = helper;
 	} 
 	
@@ -34,8 +33,8 @@ public class ArticlesSource {
 	/** Saves the title and URL, returns an object with the newly created id **/
 	public ArticleItem createArticleItem (String title, Uri url) {
 		ContentValues values = new ContentValues();
-		values.put(SavedArticlesHelper.COLUMN_TITLE, title);
-		values.put(SavedArticlesHelper.COLUMN_URL, url.toString());
+		values.put(SavedArticlesHelper.ARTICLES_COLUMN_TITLE, title);
+		values.put(SavedArticlesHelper.ARTICLES_COLUMN_URL, url.toString());
 		
 		long id = database.insert(SavedArticlesHelper.TABLE_ARTICLES, null, values);
 		
@@ -43,30 +42,38 @@ public class ArticlesSource {
 	}
 	
 	public Boolean articleExists(Uri url) {
-		List<ArticleItem> articles = this.getAllArticles();
-		
-		for(ArticleItem article : articles) {
-			if(article.url.equals(url)) {
-				return true;
-			}
-		}
-		
-		return false;
+		return helper.recordExists(database, url);
 	}
 	
 	public void removeArticle(ArticleItem item) {
-		database.delete(SavedArticlesHelper.TABLE_ARTICLES, SavedArticlesHelper.COLUMN_ID + " = " + item.id, null);
+		database.delete(SavedArticlesHelper.TABLE_ARTICLES, SavedArticlesHelper.ARTICLES_COLUMN_ID + " = " + item.id, null);
+	}
+	
+	public ArticleItem getArticleItem(Integer id) {
+		Cursor c = database.query(SavedArticlesHelper.TABLE_ARTICLES, infoColumns, SavedArticlesHelper.ARTICLES_COLUMN_ID + " = ?", new String[] {id.toString()}, null, null, null);
+		c.moveToFirst();
+		ArticleItem item = cursorToArticleItem(c);
+		c.close();
+		return item;
+	}
+	
+	public ArticleItem getArticleItem(Uri url) {
+		Cursor c = database.query(SavedArticlesHelper.TABLE_ARTICLES, infoColumns, SavedArticlesHelper.ARTICLES_COLUMN_URL + " = ?", new String[] {url.toString()}, null, null, null);
+		c.moveToFirst();
+		ArticleItem item = cursorToArticleItem(c);
+		c.close();
+		return item;
 	}
 	
 	/** Returns all articles in the database **/
-	public List<ArticleItem> getAllArticles() {
+	public List<ArticleItem> getAllArticleItems() {
 		List<ArticleItem> items = new ArrayList<ArticleItem>();
 		
-		Cursor cursor = database.query(SavedArticlesHelper.TABLE_ARTICLES, allColumns, null, null, null, null, null);
+		Cursor cursor = database.query(SavedArticlesHelper.TABLE_ARTICLES, infoColumns, null, null, null, null, null);
 		
 		cursor.moveToFirst();
 		while(!cursor.isAfterLast()) {
-			ArticleItem item = cursorToArticle(cursor);
+			ArticleItem item = cursorToArticleItem(cursor);
 			items.add(item);
 			cursor.moveToNext();
 		}
@@ -75,7 +82,7 @@ public class ArticlesSource {
 		return items;
 	}
 	
-	private ArticleItem cursorToArticle(Cursor cursor) {
+	private ArticleItem cursorToArticleItem(Cursor cursor) {
 		long id = cursor.getLong(0);
 		String title = cursor.getString(1);
 		String url = cursor.getString(2);
