dc49174f8bffc6d890ea45ff37c825cad218b8e0
Simon Strassl <simon.strassl@gmail.com>
2012-08-20 18:13:08 +0200

    Clean up SavedArticlesHelper & add new helper functions

--- Lampshade/src/eu/prismsw/lampshade/database/SavedArticlesHelper.java
+++ Lampshade/src/eu/prismsw/lampshade/database/SavedArticlesHelper.java
@@ -1,25 +1,27 @@
 package eu.prismsw.lampshade.database;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
 
 /** Helper class for the database */
 public class SavedArticlesHelper extends SQLiteOpenHelper {
 	
 	public static final String TABLE_ARTICLES = "articles";
-	public static final String COLUMN_ID = "_id";
-	public static final String COLUMN_TITLE = "title";
-	public static final String COLUMN_URL = "url";
+	public static final String ARTICLES_COLUMN_ID = "_id";
+	public static final String ARTICLES_COLUMN_TITLE = "title";
+	public static final String ARTICLES_COLUMN_URL = "url";
 	
 	protected static final String DATABASE_NAME = "saved_articles.db";
 	protected static final int DATABASE_VERSION = 1;
 	
 	protected static final String DATABASE_CREATE = "create table " + TABLE_ARTICLES + "( "
-					+ COLUMN_ID + " integer primary key autoincrement, "
-					+ COLUMN_TITLE + " text not null, "
-					+ COLUMN_URL + " text not null" + ");";
+					+ ARTICLES_COLUMN_ID + " integer primary key autoincrement, "
+					+ ARTICLES_COLUMN_TITLE + " text not null, "
+					+ ARTICLES_COLUMN_URL + " text not null" + ");";
 	
 
 	public SavedArticlesHelper(Context context) {
@@ -40,4 +42,19 @@ public class SavedArticlesHelper extends SQLiteOpenHelper {
 		db.execSQL("DROP TABLE IF EXISTS " + TABLE_ARTICLES);
 		onCreate(db);
 	}
+	
+	public Boolean recordExists(SQLiteDatabase db, Long id) {
+		Cursor c = db.query(TABLE_ARTICLES, new String[] {ARTICLES_COLUMN_ID}, ARTICLES_COLUMN_ID + "= ?", new String[] {id.toString()}, null, null, null);
+		Boolean exists = (c.getCount() > 0);
+		c.close();
+		return exists;
+	}
+	
+	public Boolean recordExists(SQLiteDatabase db, Uri url) {
+		Cursor c = db.query(TABLE_ARTICLES, new String[] {ARTICLES_COLUMN_ID}, ARTICLES_COLUMN_URL + "= ?", new String[] {url.toString()}, null, null, null);
+		Boolean exists = (c.getCount() > 0);
+		c.close();
+		return exists;
+	}
+
 }
