4512cd0e8d845b4bae85921dc247ea1b8578f03f
Simon Strassl <simon.strassl@gmail.com>
2013-04-20 14:02:24 +0200

    Refactor saved articles into a fragment

--- lampshade/src/main/java/eu/prismsw/lampshade/SavedArticlesActivity.java
+++ lampshade/src/main/java/eu/prismsw/lampshade/SavedArticlesActivity.java
@@ -12,9 +12,12 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragment;
 import com.actionbarsherlock.view.MenuItem;
 
 import eu.prismsw.lampshade.database.ArticleItem;
+import eu.prismsw.lampshade.fragments.SavedArticlesFragment;
+import eu.prismsw.lampshade.fragments.SearchFragment;
 import eu.prismsw.lampshade.listeners.OnRemoveListener;
 import eu.prismsw.lampshade.tasks.RemoveArticleTask;
 import eu.prismsw.tools.android.UIFunctions;
@@ -22,8 +25,8 @@ import eu.prismsw.tools.android.UIFunctions;
 
 /** Shows a list of saved articles */
 public class SavedArticlesActivity extends BaseActivity implements OnRemoveListener {
-	RemoveActionMode removeActionMode;
-	
+    SavedArticlesFragment listFragment;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,40 +35,14 @@ public class SavedArticlesActivity extends BaseActivity implements OnRemoveListe
 		ActionBar ab = getSupportActionBar();
 		ab.setHomeButtonEnabled(true);
 		ab.setDisplayHomeAsUpEnabled(true);
-		
-		// Prepare the ListView
-		loadArticles();
-		ListView lv = getListView();
-		
-		lv.setOnItemClickListener(new OnItemClickListener() {
-			
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				// Remove the article from the database and load it
-				ListView lv = getListView();
-				ArticleItem item = (ArticleItem) lv.getAdapter().getItem(position);
-				new RemoveArticleTask(application.savedArticlesSource, SavedArticlesActivity.this).execute(item.url);
-				application.loadPage(item.url);
-			}
-			
-		});
-		
-		this.removeActionMode = new RemoveActionMode(this, application.savedArticlesSource);
-		
-		registerForContextMenu(lv);
-		lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-		
-		lv.setOnItemLongClickListener(new OnItemLongClickListener() {
 
-			@Override
-			public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-				// Start the removeActionMode for the selected item
-				ArticleItem item = (ArticleItem) getListView().getAdapter().getItem(position);
-				removeActionMode.startActionMode(item.url);
-				return true;
-			}
-			
-		});
-	}
+        if(savedInstanceState == null) {
+            listFragment = SavedArticlesFragment.newInstance();
+
+            getSupportFragmentManager().beginTransaction().add(android.R.id.content, listFragment).commit();
+        }
+
+    }
 	
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -78,31 +55,15 @@ public class SavedArticlesActivity extends BaseActivity implements OnRemoveListe
         	return super.onOptionsItemSelected(item);
         }
     }
-    
-    public ListView getListView() {
-		ListView lv = (ListView) findViewById(R.id.lv_saved_articles);
-		return lv;
-    }
-	
-	/** Loads the saved articles from the database and sets the ArrayAdapter */
-	private void loadArticles() {
-		application.savedArticlesSource.open();
-		List<ArticleItem> savedArticles = application.savedArticlesSource.getAllArticleItems();
-		application.savedArticlesSource.close();
-
-		ArrayAdapter<ArticleItem> adapter = new ArrayAdapter<ArticleItem>(this, android.R.layout.simple_list_item_1, savedArticles);
-		getListView().setAdapter(adapter);
-	}
 
 	@Override
 	public void onRemoveSuccess(ArticleItem item) {
-		loadArticles();
 		UIFunctions.showToast(getResources().getString(R.string.article_removed) + item.title, this);
+        listFragment.reloadList();
 	}
 
 	@Override
 	public void onRemoveError() {
-		loadArticles();
 		UIFunctions.showToast(getResources().getString(R.string.article_remove_failed),  this);
 	}
 }
---
+++ lampshade/src/main/java/eu/prismsw/lampshade/fragments/SavedArticlesFragment.java
@@ -0,0 +1,118 @@
+package eu.prismsw.lampshade.fragments;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+import eu.prismsw.lampshade.R;
+import eu.prismsw.lampshade.RemoveActionMode;
+import eu.prismsw.lampshade.TropesApplication;
+import eu.prismsw.lampshade.database.ArticleItem;
+import eu.prismsw.lampshade.listeners.OnRemoveListener;
+import eu.prismsw.lampshade.tasks.RemoveArticleTask;
+
+
+public class SavedArticlesFragment extends SherlockFragment {
+    public RemoveActionMode removeActionMode;
+
+    public TropesApplication application;
+    public OnRemoveListener removeListener;
+
+    public static SavedArticlesFragment newInstance() {
+        SavedArticlesFragment f = new SavedArticlesFragment();
+        return f;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        this.application = (TropesApplication) activity.getApplication();
+        this.removeListener = (OnRemoveListener) activity;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup group, Bundle bundle) {
+        return inflater.inflate(R.layout.saved_articles_fragment, group, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        reloadList();
+    }
+
+    public void reloadList() {
+        List<ArticleItem> savedArticles = loadArticles();
+        ListView lv = getListView();
+
+        ArrayAdapter<ArticleItem> adapter = new ArrayAdapter<ArticleItem>(getSherlockActivity(), android.R.layout.simple_list_item_1, savedArticles);
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Remove the article from the database and load it
+                ListView lv = getListView();
+                ArticleItem item = (ArticleItem) lv.getAdapter().getItem(position);
+                new RemoveArticleTask(application.savedArticlesSource, removeListener).execute(item.url);
+                application.loadPage(item.url);
+            }
+
+        });
+
+        this.removeActionMode = new RemoveActionMode(getSherlockActivity(), application.savedArticlesSource);
+
+        registerForContextMenu(lv);
+        lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        lv.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                // Start the removeActionMode for the selected item
+                ArticleItem item = (ArticleItem) getListView().getAdapter().getItem(position);
+                removeActionMode.startActionMode(item.url);
+                return true;
+            }
+
+        });
+    }
+
+    private List<ArticleItem> loadArticles() {
+        TropesApplication application = (TropesApplication) getSherlockActivity().getApplication();
+
+        application.savedArticlesSource.open();
+        List<ArticleItem> savedArticles = application.savedArticlesSource.getAllArticleItems();
+        application.savedArticlesSource.close();
+
+        return savedArticles;
+    }
+
+    private ListView getListView() {
+        ListView lv = (ListView) getSherlockActivity().findViewById(R.id.lv_saved_articles);
+        return lv;
+    }
+}
