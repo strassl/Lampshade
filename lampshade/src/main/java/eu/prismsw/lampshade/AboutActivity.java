4cfae8b6643e272e1ed3aeb63245a60f1a31a15f
Simon Strassl <simon.strassl@gmail.com>
2013-05-13 19:57:17 +0200

    Add version info to AboutActivity

--- lampshade/src/main/java/eu/prismsw/lampshade/AboutActivity.java
+++ lampshade/src/main/java/eu/prismsw/lampshade/AboutActivity.java
@@ -1,10 +1,11 @@
 package eu.prismsw.lampshade;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
-
 import com.actionbarsherlock.view.MenuItem;
 
 public class AboutActivity extends BaseActivity {
@@ -15,6 +16,7 @@ public class AboutActivity extends BaseActivity {
 		
 		getSupportActionBar().setHomeButtonEnabled(true);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        addExtraInfo();
 		makeLinksClickable();
 	}
 	
@@ -25,6 +27,19 @@ public class AboutActivity extends BaseActivity {
 		TextView tvLinks = (TextView) findViewById(R.id.tv_links);
 		tvLinks.setMovementMethod(LinkMovementMethod.getInstance());
 	}
+
+    private void addExtraInfo() {
+        TextView tvSubtitle = (TextView) findViewById(R.id.tv_subtitle);
+        String versionString = "";
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            versionString = "\nv" + pInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        tvSubtitle.setText(tvSubtitle.getText() + versionString);
+    }
 	
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
